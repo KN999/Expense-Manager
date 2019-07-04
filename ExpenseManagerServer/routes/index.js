7378458965ab32fe2a682ef46d516b4c42fd2728
Navin <nk02031999@gmail.com>
2019-07-04 22:06:06 +0530

    routing still needs to be done

--- ExpenseManagerServer/routes/index.js
+++ ExpenseManagerServer/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+//var app = express();
+
+/*app.post('/route', function(req, res) {
+  var post = req.body;
+  console.log("Message Recieved");
+  res.send("sending some messages");
+});*/
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/post', function(req, res) {
+  var user_id = req.body.id;
+  var token = req.body.token;
+  var geo = req.body.geo;
+  console.log("POST request : " + user_id +" "+ token +" "+ geo)
+  res.send(user_id + ' ' + token + ' ' + geo);
 });
 
+
+router.get('/', function(req, res) {
+  var user_id = req.param('id');
+  var token = req.param('token');
+  var geo = req.param('geo');  
+  console.log("GET request was made")
+  res.send(user_id + ' ' + token + ' ' + geo);
+});
 module.exports = router;
--- ExpenseManagerServer/routes/users.js
+++ ExpenseManagerServer/routes/users.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var bodyParser = require('body-parser');
+router.use(bodyParser.json())
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/signin', function(req, res, next) {
+  var username = req.body["username"];
+  var password = req.body.password;
+  console.log("Request Body: "+ req.body + username);
+
+  if(password === "password") {
+    res.send('Sign-in successful.'+"username is "+req.body+"password is "+password);
+  }
+  else {
+    res.send('Sorry, wrong password!')
+  }
+});
+
+router.post('/signup', function(req, res, next) {
   res.send('respond with a resource');
 });
 
